f3a8cde771606dc045178b3e0975a65354017e91
Carlos Requena López <crequena@ulb.ac.be>
2017-04-20 15:59:17 +0200

    all sections. Not all content

--- source/assets/js/sdk.js
+++ source/assets/js/sdk.js
@@ -2,6 +2,6 @@
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.3&appId=1413246422256046";
+  js.src = "//connect.facebook.net/fr_FR/sdk.js#xfbml=1&version=v2.9";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
